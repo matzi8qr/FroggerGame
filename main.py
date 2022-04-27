ef35cdd88177cd726a72e7909711b8cf8d2501c0
matzi8qr <tyler.wormald@gmail.com>
2022-04-27 14:20:34 -0400

    succesfully changed method of map generation

--- main.py
+++ main.py
@@ -19,15 +19,14 @@ class Window(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        self.ROWS = self.create_rows()
+        self.init_map()
 
     def on_draw(self):
         self.clear()
         for row in self.ROWS:
-            arcade.draw_rectangle_filled(row[0], row[1], row[2], row[3], row[4])
+            row.draw()
 
     def on_update(self, delta_time):
         #shh
@@ -37,35 +36,35 @@ class Window(arcade.Window):
         pass
 
     #Boil down the attribute of the map into simplest form: a list of rows.
-    def create_rows(self):
-        rows = []
-        colors = [self.SAFE, self.ROAD, self.RIVER]
-        y_interval = SCREEN_HEIGHT/20
+    def init_map(self):
+        for i in range(3):
+            self.generate_row([self.SAFE])
+        self.generate_row([self.ROAD])
+        next = self.generate_row([self.ROAD])
+        for i in range(5):
+            next = self.generate_row(next)
+        return next
 
-        #randomly generate
-        for row_num in range(10):
-            my_color = random.choice(colors)
-            row_rect_params = [SCREEN_WIDTH/2, y_interval * (2 * row_num + 1), 
-                                SCREEN_WIDTH, 2 * y_interval, my_color]
-            rows.append(row_rect_params)
-
+    # Generates the next row based off of the passed possibilities, and returning the next possibilities.
+    def generate_row(self, possibilities):
+        #generate row based on colors possibilities
+        y_interval = SCREEN_HEIGHT/20.
+        random_color = random.choice(possibilities)
+        y_coord = y_interval * (2 * self.ROWS.__len__() + 1)
+        row_rect = arcade.create_rectangle_filled(SCREEN_WIDTH/2., y_coord, SCREEN_WIDTH, 2 * y_interval, random_color)
+        self.ROWS.append(row_rect)
         
-        #"fix" map, make sure to start with 3 safe spaces, make sure roads generate in pairs...
-        # start with 3 Safes
-        for row_num in range(3):
-            rows[row_num][4] = self.SAFE
-        # next, go to road
-        rows[3][4] = self.ROAD
-        # make sure roads generate in pairs
-        num_roads = 1
-        for row in rows[4:]:
-            if row[4] == self.ROAD:
-                num_roads += 1
-            elif num_roads != 0:    
-                row[4] = self.ROAD
-                num_roads -= 1
+        #return possibilities for next row
+        if random_color == self.SAFE:
+            return [self.ROAD, self.RIVER]
+        if possibilities == [self.ROAD]:
+            return [self.SAFE, self.ROAD]
+        if random_color == self.ROAD:
+            return [self.ROAD]
+        if random_color == self.RIVER:
+            return [self.RIVER, self.SAFE]
 
-        return rows
+
 
 
 if __name__ == "__main__":
